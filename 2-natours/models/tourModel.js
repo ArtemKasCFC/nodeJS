78b7fe03566946065702ae895a844a74cabd82fb
Artem Kasperovich <artem.kas.cfc@gmail.com>
2023-10-18 19:25:59 +0300

    some validation is added

--- 2-natours/models/tourModel.js
+++ 2-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour must have less or equal then 40 characters'],
+      minlength: [10, 'A tour must have more or equal then 10 characters'],
+      // validate: [validator.isAlpha, 'Name must contain only characters'],
     },
     slug: String,
     duration: {
@@ -22,10 +26,13 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a difficulty'],
       trim: true,
+      enum: { values: ['easy', 'medium', 'difficult'], message: 'Difficulty can be easy, medium or difficult' },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      max: [5, 'Rating must be below 5.0'],
+      min: [1, 'Rating must be above 1.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +42,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val <= this.price;
+        },
+        message: 'Discount ({VALUE}) must be less or equal then price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
